60cf41eb60b8889495750a5a5c2de353865a6a61
DESKTOP-G3E1MO0\ScorpionIPX <scorpionipx@gmail.com>
2017-05-27 12:56:56 +0300

    Updated models and added first entries in DB

--- ChelmusBooking/Apps/BookingManager/admin.py
+++ ChelmusBooking/Apps/BookingManager/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from . import models
 
 EMPTY_VALUE = '-empty-'
 
@@ -11,3 +12,18 @@ class PostTypeAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'description', )
     ordering = ['name', ]
     empty_value_display = EMPTY_VALUE
+    
+
+class PostAdmin(admin.ModelAdmin):
+    """
+        Post's meta for admin site.
+    """
+    list_display = ('pk', 'name', 'post_type', 'description', 'address', 'stars', 'price', 'rating', 'visitors_no', 'posted_on', )
+    ordering = ['name', ]
+    empty_value_display = EMPTY_VALUE
+    
+    
+# register entries
+
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.PostType, PostTypeAdmin)
--- ChelmusBooking/Apps/BookingManager/models.py
+++ ChelmusBooking/Apps/BookingManager/models.py
@@ -34,11 +34,11 @@ class Post(models.Model):
     name = models.CharField(max_length=100, verbose_name='Name', unique=True)
     post_type = models.ForeignKey(PostType, on_delete=models.CASCADE, related_name='type')
     description = models.TextField(max_length=1000, verbose_name='Description', blank=True, null=False, default=DEFAULT_DESCRIPTION)
-    address = models.TextField(max_length=250, verbose_name='Address', blank=True, null=False)
-    stars = models.CharField(max_length=10, verbose_name='Stars', unique=True)
+    address = models.TextField(max_length=250, verbose_name='Contact', blank=True, null=False)
+    stars = models.CharField(max_length=10, verbose_name='Stars', unique=True, default = '1')
     price = models.CharField(max_length=15, verbose_name='Price', unique=True)
-    rating = models.CharField(max_length=10, verbose_name='Rating', unique=True)
-    visitors_no = models.CharField(max_length=10, verbose_name='Number of visitors', unique=True)
+    rating = models.CharField(max_length=10, verbose_name='Rating', unique=True, default='0.0')
+    visitors_no = models.CharField(max_length=10, verbose_name='Number of visitors', unique=True, default='0')
     posted_on = models.DateTimeField(auto_now_add=True, editable=False, blank=True, null=True)
     
     class Meta:
